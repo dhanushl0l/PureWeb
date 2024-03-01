b4030d7a3a9d0ce3b605122ace4a5b365317e264
dhanushl0l <dhanushdjtherock@gmail.com>
2024-03-01 21:37:10 +0530

    buttons and social media links js fix

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav ul li a");
-  
-    links.forEach(function(link) {
-      link.addEventListener("click", function(event) {
-        event.preventDefault();
-  
-        const targetId = this.getAttribute("href").substring(1);
-        const targetSection = document.getElementById(targetId);
-  
-        document.querySelectorAll(".section").forEach(function(section) {
-          section.classList.remove("active");
-        });
-  
-        targetSection.classList.add("active");
-      });
+// Add nav section selector 
+const links = document.querySelectorAll("nav ul li a");
+
+links.forEach(function(link) {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    document.querySelectorAll(".section").forEach(function(section) {
+      section.classList.remove("active");
     });
+
+    targetSection.classList.add("active");
   });
-  
+});
+
+// Add event listeners for the "RESUME" and "PROJECTS" buttons
+document.querySelector(".about-button-1").addEventListener("click", function() {
+  const resumeSection = document.getElementById("resume");
+  document.querySelectorAll(".section").forEach(function(section) {
+    section.classList.remove("active");
+  });
+  resumeSection.classList.add("active");
+});
+
+document.querySelector(".about-button-2").addEventListener("click", function() {
+  const projectsSection = document.getElementById("projects");
+  document.querySelectorAll(".section").forEach(function(section) {
+    section.classList.remove("active");
+  });
+  projectsSection.classList.add("active");
+});
